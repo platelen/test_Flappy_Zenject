36b15198d7f9b2acfd13957d7ae4cd29d54768b8
Dmitry <platelen@gmail.com>
2024-01-30 13:34:28 +0300

    Ref
    Ref.

--- Assets/Scripts/GameOver/ScoringAfterGameOver.cs
+++ Assets/Scripts/GameOver/ScoringAfterGameOver.cs
@@ -3,17 +3,16 @@ using UnityEngine;
 
 namespace GameOver
 {
-    public class ScoringAfterGameOver:MonoBehaviour
+    public class ScoringAfterGameOver : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _currentScore;
         [SerializeField] private TextMeshProUGUI _bestScore;
 
-        // Счет уже сохраненный, храните его где-то в игре
         private int bestScore;
 
         private void Start()
         {
-            bestScore = GetBestScoreFromStorage(); // Загрузите лучший счет из сохраненных данных
+            bestScore = GetBestScoreFromStorage();
             _bestScore.text = bestScore.ToString();
         }
 
@@ -25,21 +24,17 @@ namespace GameOver
             {
                 bestScore = score;
                 _bestScore.text = bestScore.ToString();
-                SaveBestScoreToStorage(); // Сохраните новый лучший счет
+                SaveBestScoreToStorage();
             }
         }
 
         public int GetBestScoreFromStorage()
         {
-            // Здесь вам нужно реализовать получение лучшего счета из сохраненных данных (например, из PlayerPrefs)
-            // Пример:
             return PlayerPrefs.GetInt("BestScore", 0);
         }
 
         private void SaveBestScoreToStorage()
         {
-            // Здесь вам нужно реализовать сохранение лучшего счета в сохраненных данных
-            // Пример:
             PlayerPrefs.SetInt("BestScore", bestScore);
             PlayerPrefs.Save();
         }
